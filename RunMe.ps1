bf21cf734a3663a3d2f920298acbe717a642d28e
Tim Rayburn <Tim@TimRayburn.net>
2013-10-05 20:13:31 -0500

    Log file structure created #4

--- RunMe.ps1
+++ RunMe.ps1
@@ -8,9 +8,44 @@
 # DO NOT MODIFY THIS FILE, MODIFY RunMeConfig.ps1 INSTEAD
 ###########################################################
 
+$LogFile = ".\RunMe.log"
+
 ###########################################################
 # FUNCTIONS
 ###########################################################
+
+function Out-Host {
+    [CmdletBinding()]
+    Param(
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)]
+    $Message)
+
+    Write-Host $Message
+    Out-Log $Message
+}
+
+function Out-Verbose {
+    [CmdletBinding()]
+    Param(
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)]
+    $Message)
+
+    Write-Verbose -Message $Message
+    Out-Log $Message
+}
+
+function Out-Log {
+    [CmdletBinding()]
+    Param(
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)]
+    $Message)
+
+    $Message >> $LogFile
+}
+
+function Success($msg) {
+    "SUCCESS : $msg"
+}
 
 function Test-IsAdmin {
     try {
@@ -26,6 +61,10 @@ function Test-IsAdmin {
 # Main Script
 ###########################################################
 
+"BEGIN : RunMe.ps1" | Out-Host
+
 if ((Test-IsAdmin) -eq $false) { 
     throw "You must be running as an Administrator" 
+} else {
+    Success("Running as Administrator") | Out-Host
 }
